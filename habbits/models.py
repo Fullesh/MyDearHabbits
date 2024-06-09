64184f54d3c0baa8c5be5140169bacb18b9c3caf
Fullesh <76729342+Fullesh@users.noreply.github.com>
2024-06-09 11:21:51 +0300

    Исправления models.py

--- habbits/models.py
+++ habbits/models.py
@@ -24,7 +24,7 @@ class Habbit(models.Model):
     revard = models.CharField(max_length=150, verbose_name='Вознаграждение')
     duration = models.DurationField(default=timedelta(minutes=2), verbose_name='Время на выполнение (минуты)')
     is_public = models.BooleanField(default=False, verbose_name='Публичная')
-    users = models.ForeignKey(settings.AUTH_USER_MODEL, on.delete=models.CASCADE, verbose_name="Пользователи",
+    users = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователи",
                               related_name="users", **NULLABLE)
 
     def __str__(self):
